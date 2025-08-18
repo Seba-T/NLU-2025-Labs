b9441864ffbecda974d6313e8aaa70bef914d387
Sebastiano Tocci <sebastiano.tocci@proton.me>
2025-08-18 17:30:47 +0200

    Refactor notebook for improved readability and consistency
    - Removed unnecessary line breaks and adjusted comments for clarity.
    - Standardized spacing in function definitions and method calls.
    - Ensured consistent formatting in print statements and data processing sections.
    - Cleaned up commented-out code for better organization.

---
+++ exam/257842_sebastiano_tocci/LM/part_A/functions.py
@@ -0,0 +1,86 @@
+# Add the class of your model only
+# Here is where you define the architecture of your model using pytorch
+import math
+import torch
+import torch.nn as nn
+
+
+# This class computes and stores our vocab
+# Word to ids and ids to word
+class Lang:
+    def __init__(self, corpus, special_tokens=[]):
+        self.word2id = self.get_vocab(corpus, special_tokens)
+        self.id2word = {v: k for k, v in self.word2id.items()}
+
+    def get_vocab(self, corpus, special_tokens=[]):
+        output = {}
+        i = 0
+        for st in special_tokens:
+            output[st] = i
+            i += 1
+        for sentence in corpus:
+            for w in sentence.split():
+                if w not in output:
+                    output[w] = i
+                    i += 1
+        return output
+
+
+def train_loop(data, optimizer, criterion, model, clip=5):
+    model.train()
+    loss_array = []
+    number_of_tokens = []
+
+    for sample in data:
+        optimizer.zero_grad()  # Zeroing the gradient
+        output = model(sample["source"])
+        loss = criterion(output, sample["target"])
+        loss_array.append(loss.item() * sample["number_tokens"])
+        number_of_tokens.append(sample["number_tokens"])
+        loss.backward()  # Compute the gradient, deleting the computational graph
+        # clip the gradient to avoid exploding gradients
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()  # Update the weights
+
+    return sum(loss_array) / sum(number_of_tokens)
+
+
+def eval_loop(data, eval_criterion, model):
+    model.eval()
+    loss_to_return = []
+    loss_array = []
+    number_of_tokens = []
+    # softmax = nn.Softmax(dim=1) # Use Softmax if you need the actual probability
+    with torch.no_grad():  # It used to avoid the creation of computational graph
+        for sample in data:
+            output = model(sample["source"])
+            loss = eval_criterion(output, sample["target"])
+            loss_array.append(loss.item())
+            number_of_tokens.append(sample["number_tokens"])
+
+    ppl = math.exp(sum(loss_array) / sum(number_of_tokens))
+    loss_to_return = sum(loss_array) / sum(number_of_tokens)
+    return ppl, loss_to_return
+
+
+def init_weights(mat):
+    for m in mat.modules():
+        if type(m) in [nn.GRU, nn.LSTM, nn.RNN]:
+            for name, param in m.named_parameters():
+                if "weight_ih" in name:
+                    for idx in range(4):
+                        mul = param.shape[0] // 4
+                        torch.nn.init.xavier_uniform_(
+                            param[idx * mul : (idx + 1) * mul]
+                        )
+                elif "weight_hh" in name:
+                    for idx in range(4):
+                        mul = param.shape[0] // 4
+                        torch.nn.init.orthogonal_(param[idx * mul : (idx + 1) * mul])
+                elif "bias" in name:
+                    param.data.fill_(0)
+        else:
+            if type(m) in [nn.Linear]:
+                torch.nn.init.uniform_(m.weight, -0.01, 0.01)
+                if m.bias != None:
+                    m.bias.data.fill_(0.01)
---
+++ exam/257842_sebastiano_tocci/LM/part_A/main.py
@@ -0,0 +1,113 @@
+# This file is used to run your functions and print the results
+# Please write your fuctions or classes in the functions.py
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import copy
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import numpy as np
+
+
+# Import everything from functions.py file
+from functions import *
+from utils import *
+from model import LM_RNN
+
+
+if __name__ == "__main__":
+    DEVICE = "cuda:0"
+
+    download_file(
+        "https://raw.githubusercontent.com/BrownFortress/NLU-2024-Labs/main/labs/dataset/PennTreeBank/ptb.test.txt"
+    )
+    download_file(
+        "https://raw.githubusercontent.com/BrownFortress/NLU-2024-Labs/main/labs/dataset/PennTreeBank/ptb.valid.txt"
+    )
+    download_file(
+        "https://raw.githubusercontent.com/BrownFortress/NLU-2024-Labs/main/labs/dataset/PennTreeBank/ptb.train.txt"
+    )
+
+    train_raw = read_file("dataset/PennTreeBank/ptb.train.txt")
+    dev_raw = read_file("dataset/PennTreeBank/ptb.valid.txt")
+    test_raw = read_file("dataset/PennTreeBank/ptb.test.txt")
+    lang = Lang(train_raw, ["<pad>", "<eos>"])
+    # Wrtite the code to load the datasets and to run your functions
+    train_dataset = PennTreeBank(train_raw, lang)
+    dev_dataset = PennTreeBank(dev_raw, lang)
+    test_dataset = PennTreeBank(test_raw, lang)
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=64,
+        collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"]),
+        shuffle=True,
+    )
+    dev_loader = DataLoader(
+        dev_dataset,
+        batch_size=128,
+        collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"]),
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=128,
+        collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"]),
+    )
+
+    # Experiment also with a smaller or bigger model by changing hid and emb sizes
+    # A large model tends to overfit
+    hid_size = 200
+    emb_size = 300
+
+    # Don't forget to experiment with a lower training batch size
+    # Increasing the back propagation steps can be seen as a regularization step
+
+    # With SGD try with an higher learning rate (> 1 for instance)
+    lr = 1  # This is definitely not good for SGD
+    clip = 5  # Clip the gradient
+
+    vocab_len = len(lang.word2id)
+
+    model = LM_RNN(emb_size, hid_size, vocab_len, pad_index=lang.word2id["<pad>"]).to(
+        DEVICE
+    )
+    model.apply(init_weights)
+
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
+    criterion_eval = nn.CrossEntropyLoss(
+        ignore_index=lang.word2id["<pad>"], reduction="sum"
+    )
+
+    n_epochs = 50
+    patience = 3
+    losses_train = []
+    losses_dev = []
+    sampled_epochs = []
+    best_ppl = math.inf
+    best_model = None
+    pbar = tqdm(range(1, n_epochs))
+    # If the PPL is too high try to change the learning rate
+    for epoch in pbar:
+        loss = train_loop(train_loader, optimizer, criterion_train, model, clip)
+        if epoch % 1 == 0:
+            sampled_epochs.append(epoch)
+            losses_train.append(np.asarray(loss).mean())
+            ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
+            losses_dev.append(np.asarray(loss_dev).mean())
+            pbar.set_description("PPL: %f" % ppl_dev)
+            if ppl_dev < best_ppl:  # the lower, the better
+                best_ppl = ppl_dev
+                best_model = copy.deepcopy(model).to("cpu")
+                patience = 3
+            else:
+                patience -= 1
+
+            if patience <= 0:  # Early stopping with patience
+                break  # Not nice but it keeps the code clean
+
+    if best_model:
+        best_model.to(DEVICE)
+
+    final_ppl, _ = eval_loop(test_loader, criterion_eval, best_model)
+    print("Test ppl: ", final_ppl)
